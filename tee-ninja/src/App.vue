330ecc9de2be93bee460172508bb100486230d35
gitchili <jameslphillips@gmail.com>
2018-06-21 01:25:23 -0600

    nav 0.1
    nav upDate 0.1

--- tee-ninja/src/App.vue
+++ tee-ninja/src/App.vue
@@ -1,6 +1,20 @@
 <template>
+   <!-- theme change = dark or light etc -->
   <v-app>
-
+    <!-- static content  nav, footer, tool bar same for all pages = none dynamic-->
+<v-toolbar>
+  <v-toolbar-title>Tee-ninja</v-toolbar-title>
+  <!-- v-spacer v-toolbar uses flexblox-->
+  <v-spacer></v-spacer>
+  <v-toolbar-items>
+    <v-btn flat>sign up</v-btn>
+        <v-btn flat>sign in</v-btn>
+  </v-toolbar-items>
+</v-toolbar>
+    <!-- dynamic content to be displayed lives in a html elemnt named main-->
+    <main>
+      
+    </main>
   </v-app>
 </template>
 
